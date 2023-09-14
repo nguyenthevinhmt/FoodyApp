dc7a204a6557ca1d236a8d0226268e03885fce7c
Nguyễn Thế Vinh <nguyenthevinhmt0x@gmail.com>
2023-09-14 11:17:49 +0700

    add delele,update, get by id controller, fix gitignore

--- FoodyApp/BE/Foody.API/Controllers/ProductManagerModule/ProductController.cs
+++ FoodyApp/BE/Foody.API/Controllers/ProductManagerModule/ProductController.cs
@@ -27,6 +27,11 @@ namespace Foody.API.Controllers.ProductManagerModule
             var result = await _service.GetProductPaging(input);
             return Ok(result);
         }
+        /// <summary>
+        /// Tạo mới sản phẩm
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
         [HttpPost("create-product")]
         public async Task<IActionResult> CreateProduct([FromForm] CreateProductDto input)
         {
@@ -40,6 +45,59 @@ namespace Foody.API.Controllers.ProductManagerModule
                 return BadRequest(ex.Message);
             }
         }
-
+        /// <summary>
+        /// Lấy sản phẩm theo id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("get-product-by-id")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            try
+            {
+                var result = await _service.GetProductById(id);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        /// <summary>
+        /// Cập nhật thông tin sản phẩm
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        [HttpPut("update-product")]
+        public async Task<IActionResult> Update([FromForm] UpdateProductDto input)
+        {
+            try
+            {
+                await _service.UpdateProduct(input);
+                return Ok("Update thành công");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        /// <summary>
+        /// Xóa thông tin sản phẩm
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpDelete("delete-product")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                await _service.DeleteProduct(id);
+                return Ok("Xóa thành công");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
     }
 }
